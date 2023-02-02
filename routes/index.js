7aa514c5040f59a35252495dea1832ca0e78f532
nahum2003 <117127512+nahum2003@users.noreply.github.com>
2023-02-02 18:58:18 -0400

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,155 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const sqlite3=require('sqlite3').verbose();
+const path = require('path');
+const geoip = require('geoip-lite');
+const nodemailer = require('nodemailer');
+const { request } = require('http');
+const fetch = require('node-fetch');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Curriculum Vitae' });
+require('dotenv').config();
+
+  
+const basededatos=path.join(__dirname,"basededatos","basededatos.db");
+const bd=new sqlite3.Database(basededatos, err =>{ 
+if (err){
+	return console.error(err.message);
+}else{
+	console.log("...");
+}
+})
+
+
+const create="CREATE TABLE IF NOT EXISTS contactos(email VARCHAR(20),nombre VARCHAR(20), comentario TEXT,fecha DATATIME,ip TEXT, pais VARCHAR(15));";
+
+bd.run(create,err=>{
+	if (err){
+	return console.error(err.message);
+}else{
+	console.log("...");
+}
+})
+
+router.get('/contactos',(req,res)=>{
+	const sql="SELECT * FROM contactos;";
+	bd.all(sql, [],(err, rows)=>{
+			if (err){
+				return console.error(err.message);
+			}else{
+			res.render("contactos.ejs",{tarea:rows});
+			}
+	})
+})
+
+
+
+
+
+router.post('/',(req,res)=>{
+  	const response_key = req.body["g-recaptcha-response"];
+  	const secret_key = process.env.SECRET_KEY;
+  	const url = 
+	`https://www.google.com/recaptcha/api/siteverify?secret=${secret_key}&response=${response_key}`;
+  	fetch(url, {
+    	method: "post",
+  	})
+    	.then((response) => response.json())
+    	.then((google_response) => {
+	//Si se verifica el captcha
+      	if (google_response.success == true) {
+        	var hoy = new Date();
+  			var horas = hoy.getHours();
+  			var minutos = hoy.getMinutes();
+			minutos = minutos < 10 ? '0' + minutos : minutos;
+  			var segundos = hoy.getSeconds();
+  			var hora = horas + ':' + minutos + ':' + segundos + ' ';
+  			var fecha = hoy.getDate() + '-' + ( hoy.getMonth() + 1 ) + '-' + hoy.getFullYear() + '//' + hora;
+	 		 var ip = req.headers["x-forwarded-for"];
+  			if (ip){
+    		var list = ip.split(",");
+    		ip= list[list.length-1];
+ 			 } else {
+	 		 ip = req.connection.remoteAddress;
+  			}
+			var geo = geoip.lookup(ip);
+			console.log(geo);
+			var pais = geo.country;
+			const sql="INSERT INTO contactos(nombre, email, comentario, fecha ,ip, pais) VALUES (?,?,?,?,?,?)";
+			const nuevos_mensajes=[req.body.nombre, req.body.email, req.body.comentario,fecha,ip,pais];
+
+
+			bd.run(sql, nuevos_mensajes, err =>{
+				if (err){
+					return console.error(err.message);
+				}
+				else{
+				res.redirect("/");
+				}
+				})
+
+				let transporter = nodemailer.createTransport({
+					host: "smtp-mail.outlook.com",
+    				secureConnection: false, 
+    				port: 587, 
+    				auth: {
+       				 user: process.env.CORREO_NM,
+       				 pass: process.env.CLAVE_CORREO_NM
+
+    				},
+    					tls: {
+      					ciphers:'SSLv3'
+   					}
+			});
+				const Message = `
+					<p>Programacion 2 Sec-1</p>
+					<h3>Información del Cliente:</h3>
+					<ul>
+					  <li>E-mail: ${req.body.email}</li>
+					  <li>Nombre: ${req.body.nombre}</li>
+					  <li>Comentario: ${req.body.comentario}</li>
+					  <li>Fecha-Hora: ${fecha}</li>
+					<li>IP: ${ip}</li>
+					<li>Pais: ${pais}</li>
+					</ul>`;
+				const receiverAndTransmitter = {
+					from: process.env.CORREO_NM,
+					to: 'programacion2ais@dispostable.com',
+					subject: 'Informacion del Contacto', 
+					html: Message
+				};
+				transporter.sendMail(receiverAndTransmitter,(err, info) => {
+					if(err)
+						console.log(err)
+					else
+						console.log(info);
+					})
+    }else{
+	//Si hay error en el captcha 
+        setTimeout(function(){ 
+			res.redirect("/");				
+		}, 1800);
+    }
+    })
+    .catch((error) => {
+    return res.json({ error });
+    });
+  	
+
 });
 
+
+
+router.get('/',(req,res)=>{
+	res.render('index.ejs',{tarea:{}})
+});
+
+
 module.exports = router;
+
+
+
+
+
+
+
+
