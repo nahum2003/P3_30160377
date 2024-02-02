b2b61e5885fb3dd47c80d2d3d70a488494d408e1
nahum2003 <117127512+nahum2003@users.noreply.github.com>
2024-02-02 17:12:50 -0400

    Update controllers.js
    claves de recaptcha

--- controllers.js
+++ controllers.js
@@ -5,7 +5,8 @@ const path = require('path');
 require('dotenv').config();
 var handlebars = require('express-handlebars')
 const Recaptcha = require('express-recaptcha').RecaptchaV2;
-const recaptcha = new Recaptcha('6LfOBUYpAAAAAN8w1Meatl-oxfr8w4EBhh2Cvsj1', '6LfOBUYpAAAAAO5ERFJseNoPrvc9X0ckgVGdPOkD');
+const recaptcha = new Recaptcha('6Le37GQpAAAAANoRVIJ9_0BlIKm8Zbmb6O8VUbIs', '6Le37GQpAAAAAN3lk-1sr2nLVmjaMaHsEQlIQyly
+');
 const multer = require('multer');
 const http = require('http');
 const express = require('express');
